aaf9ae1f8f3127cf715f07194e7289f6d899dbb2
Tomasz Sójka <tomek@air-tomasz.home>
2024-06-05 22:58:58 +0200

    create responsive game selection layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    container: {
-      center: true,
-      padding: "1.5rem",
-      screens: {
-        xl: "1920px",
-      },
-    },
   },
   plugins: [],
 };
